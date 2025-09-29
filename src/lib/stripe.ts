3d9bab41bfba059fb0dbea3e460fd57197b4bbb8
ujjwal-codementor <ujjwal.codementor@gmail.com>
2025-09-29 23:39:59 +0530

    removed api version of stripe

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -6,7 +6,7 @@ if (!process.env.STRIPE_SECRET_KEY) {
 }
 
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-  apiVersion: '2025-07-30.basil',
+  // apiVersion: '2025-07-30.basil',
 });
 
 // Re-export client-side constants for server-side use
